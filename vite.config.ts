1c73ffbf405fb1257f8d7fd99017d638f8f21984
ifaz <ifaz.farha@gmail.com>
2025-04-11 22:34:59 +0600

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   base: '/portfolio/', // Update this to match your repository name
+
+  base: '/portfolio_3/', // Update this to match your repository name
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
